1dbc2c8b0e64de4ce5fb4ea8a67cd87b963e1107
Andres Manco <andresmanco@gmail.com>
2018-06-13 12:05:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector){
+  var s = document.querySelector(selector)
+  return s;
+}
+
+function nestedTarget(){
+  var n = document.getElementById('nested').querySelector('div.target')
+  return n;
+}
+
+function increaseRankBy(n){
+  var rank = document.querySelectorAll('ul.ranked-list')
+  for(var i = 0; i < rank.length; i++){
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
+  }
+  return rank;
+}
+
+function deepestChild(){
+  var deep = document.getElementById('grand-node').querSelectorAll("")
+  
+  return deep;
+}
